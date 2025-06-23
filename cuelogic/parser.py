cf88ce8831e509cf1a04247c6cfe7699a09d594d
Olezhich <qwertypx@yandex.ru>
2025-06-23 23:25:59 +0300

    added new logic of line processing in parser

--- cuelogic/parser.py
+++ cuelogic/parser.py
@@ -3,12 +3,20 @@ import os.path
 from cuelogic import AlbumData, TrackData
 
 import shlex
+import re
+
+line_pattern = re.compile(r'"([^"]+)"|([^,"]+)')
 
 def extract_word(line : str, n : int) -> str | None:
     try:
         return shlex.split(line)[n]
     except (ValueError, IndexError):
         return None
+
+def process_line(line : str, case : str, many : bool=False) -> list[str]:
+    line = line[len(case):]
+    tokens = line.split(',') if many else [line]
+    return [i.strip().strip('\"\'') for i in tokens]
 
 def load(cue : str) -> AlbumData:
     album = AlbumData()
@@ -18,29 +26,30 @@ def load(cue : str) -> AlbumData:
 
     for line in cue.splitlines():
         line = line.strip()
+
         if line.startswith("PERFORMER") and not current_track:
             album.set_performer(extract_word(line, 1))
         elif line.startswith("TITLE") and not current_track:
-            album.set_title(extract_word(line, 1))
+            album.set_title(process_line(line, "TITLE")[0])
         elif line.startswith("FILE"):
-            file_path = dir + extract_word(line, 1)
+            file_path = dir + process_line(line, "FILE")[0]
             current_file = file_path
         elif line.startswith('REM GENRE'):
-            album.set_genre(extract_word(line, 2))
+            album.set_genre(process_line(line, "REM GENRE")[0])
         elif line.startswith('REM DATE'):
-            album.set_date(extract_word(line, 2))
+            album.set_date(process_line(line, "REM DATE")[0])
 
         elif line.startswith("TRACK"):
             if current_track:
                 album.add_track(current_track)
-            current_track = TrackData(track=extract_word(line, 1), link=current_file)
+            current_track = TrackData(track=process_line(line, "TRACK")[0], link=current_file)
 
         elif line.startswith("TITLE") and current_track:
-            current_track.set_title(extract_word(line, 1))
+            current_track.set_title(process_line(line, "TITLE")[0])
         elif line.startswith("PERFORMER") and current_track:
-            current_track.set_performer(extract_word(line, 1))
+            current_track.set_performer(process_line(line, "PERFORMER")[0])
         elif line.startswith("INDEX") and current_track:
-            current_track.add_index((extract_word(line, 1), extract_word(line, 2)))
+            current_track.add_index((process_line(line, "INDEX")[0], process_line(line, "INDEX")[1]))
     if current_track:
         album.add_track(current_track)
 
@@ -48,3 +57,10 @@ def load(cue : str) -> AlbumData:
 
 
     return album
+
+if __name__ == '__main__':
+    print('___process_line_tests___')
+    print(process_line('REM DATE 1969', 'REM DATE'))
+    print(process_line('REM DATE "1969"', 'REM DATE'))
+    print(process_line('REM DATE 1969, 1975', 'REM DATE', many=True))
+    print(process_line('REM DATE "1969", "1975"', 'REM DATE', many=True))
